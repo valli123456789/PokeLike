3b1554b06b2962949d113764ff60c33ba9d58669
TobysHP <36991491+TobysHP@users.noreply.github.com>
2018-09-21 23:45:21 +0200

    Add files via upload

--- PokeLike/PokeLike/Dresseur.cpp
+++ PokeLike/PokeLike/Dresseur.cpp
@@ -2,8 +2,9 @@
 
 Dresseur::Dresseur()
 {
-	d_texture.loadFromFile("Sprite/sacha.png", sf::IntRect(0, 0, 32, 48));
+	d_texture.loadFromFile("Sprite/golbez.png", sf::IntRect(0, 0, 32, 48));
 	d_sprite.setTexture(d_texture);
+	d_sprite.setPosition(600, 600);
 }
 
 
@@ -11,24 +12,37 @@ Dresseur::~Dresseur()
 {
 }
 
-void Dresseur::update()//Définition de la fonction update (qui est maintenant une fonction  membre de la classe "Dresseur")
+void Dresseur::update(bool t)//Définition de la fonction update (qui est maintenant une fonction  membre de la classe "Dresseur")
 {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) // Vérifie si la flèche "Down" du clavier est appuyé
 	{
-		d_sprite.move(0, 2);// Dans le cas ou la condition du "if" est vérifiée La fonction "move" déplace le sprite selon le vecteur (x,y) sachant que l'axe -y est dirigé vers le bas et l'axe -x vers la droite
+		if (t)
+			d_sprite.move(0, 3);// Dans le cas ou la condition du "if" est vérifiée La fonction "move" déplace le sprite selon le vecteur (x,y) sachant que l'axe -y est dirigé vers le bas et l'axe -x vers la droite
+		else
+			d_sprite.move(0, -2);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
 	{
-		d_sprite.move(-2, 0);
+		if (t)
+			d_sprite.move(-3, 0);
+		else
+			d_sprite.move(2, 0);
+
 	}
 
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
 	{
-		d_sprite.move(2, 0);
+		if (t)
+			d_sprite.move(3, 0);
+		else
+			d_sprite.move(2, 0);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
 	{
-		d_sprite.move(0, -2);
+		if (t)
+			d_sprite.move(0, -3);
+		else
+			d_sprite.move(0, 2);
 	}
 
 }
