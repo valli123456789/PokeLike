7098e688728a4fcea46e7c4d5c11a85174807c07
Draffaco017 <43415865+Draffaco017@users.noreply.github.com>
2018-09-22 23:42:51 +0200

    Update Pokemon.h

--- PokeLike/PokeLike/Pokemon.h
+++ PokeLike/PokeLike/Pokemon.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "Attaque.h"
 
 class Pokemon
 {
@@ -8,6 +9,17 @@ public:
 	Pokemon();
 	~Pokemon();
 	sf::Sprite p_sprite;
+	sf::String p_getnom();
+	sf::String p_gettype();
+	int p_getpv();
+	int p_getatk();
+	int p_getatkspe();
+	int p_getdef();
+	int p_getdefspe();
+	int p_getvit();
+	int p_getrare();
+	int p_getevdonne();
+	std::string p_gettypeev();
 
 private:
 	sf::Texture p_texture;
@@ -21,4 +33,7 @@ private:
 	int p_defspe;
 	int p_vit;
 	int p_rare;
+	int p_evdonne;
+	std::string p_typeev;
+	Attaque p_listeatq[4];
 };
