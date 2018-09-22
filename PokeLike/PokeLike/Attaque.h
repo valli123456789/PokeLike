233c287398b4b55c3bb33b07fe4fc6188610722d
Draffaco017 <43415865+Draffaco017@users.noreply.github.com>
2018-09-22 22:03:42 +0200

    Update Attaque.h

--- PokeLike/PokeLike/Attaque.h
+++ PokeLike/PokeLike/Attaque.h
@@ -19,6 +19,7 @@ string a_nom;
 string type;
 int a_puissance;
 string a_stataffectee;
+float a_precision;
 //pourquoi ne pas créer une variable booléen par type d'attaque au lieu du typecode, niveau mémoire c'est un peu mieux qu'un gros int
 //genre un truc ainsi, qui serait stocké dans la database, au lieu du typecode,  des interrupteurs différents??? : 
 /*
