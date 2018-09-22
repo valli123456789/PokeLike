062120edaca330ea8e44be3e395ce7d934eb8481
Draffaco017 <43415865+Draffaco017@users.noreply.github.com>
2018-09-22 23:43:10 +0200

    Update Pokemon.cpp

--- PokeLike/PokeLike/Pokemon.cpp
+++ PokeLike/PokeLike/Pokemon.cpp
@@ -10,3 +10,48 @@ Pokemon::Pokemon()
 Pokemon::~Pokemon()
 {
 }
+
+sf::String Pokemon::p_getnom()
+{
+	return p_nom;
+}
+sf::String Pokemon::p_gettype()
+{
+	return p_type;
+}
+int Pokemon::p_getpv()
+{
+	return p_pv;
+}
+int Pokemon::p_getatk()
+{
+	return p_atk;
+}
+int Pokemon::p_getatkspe()
+{
+	return p_atkspe;
+}
+int Pokemon::p_getdef()
+{
+	return p_def;
+}
+int Pokemon::p_getdefspe()
+{
+	return p_defspe;
+}
+int Pokemon::p_getvit()
+{
+	return p_vit;
+}
+int Pokemon::p_getrare()
+{
+	return p_rare;
+}
+int Pokemon::p_getevdonne()
+{
+	return p_evdonne;
+}
+std::string Pokemon::p_gettypeev()
+{
+	return p_typeev;
+}
