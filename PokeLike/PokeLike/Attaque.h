7d92008d01168d12a544eddd994906d4f4984c1f
Draffaco017 <43415865+Draffaco017@users.noreply.github.com>
2018-09-22 23:42:31 +0200

    Update Attaque.h

--- PokeLike/PokeLike/Attaque.h
+++ PokeLike/PokeLike/Attaque.h
@@ -1,47 +1,46 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <windows.h>
+class Attaque {
 
-class Attaque{
-
-public :
-Attaque();
-~Attaque();
-string a_getnom();
-string a_gettype();
-int a_getpuissance();
-string a_getstataffectee();
-float a_getprecision();
+public:
+	Attaque();
+	~Attaque();
+	std::string a_getnom();
+	std::string a_gettype();
+	int a_getpuissance();
+	std::string a_getstataffectee();
+	float a_getprecision();
 
 
-private :
-int a_id;
-string a_nom;
-string type;
-int a_puissance;
-string a_stataffectee;//def(spe), atk(spe), vit, precis, crit????
-float a_precision;
-//pourquoi ne pas créer une variable booléen par type d'attaque au lieu du typecode, niveau mémoire c'est un peu mieux qu'un gros int
-//genre un truc ainsi, qui serait stocké dans la database, au lieu du typecode,  des interrupteurs différents??? : 
-/*
-bool a_normalok;
-bool a_fireok;
-bool a_fightingok;
-bool a_waterok;
-bool a_flyingok;
-bool a_grassok;
-bool a_poison;
-bool a_electric;
-bool a_ground;
-bool a_psychic;
-bool a_rock;
-bool a_ice;
-bool a_bug;
-bool a_dragon;
-bool a_ghost;
-bool a_dark;
-bool asteel;
-bool a_fairy;
-*/
+private:
+	int a_id;
+	std::string a_nom;
+	std::string a_type;
+	int a_puissance;
+	std::string a_stataffectee;//def(spe), atk(spe), vit, precis, crit????
+	float a_precision;
+	//pourquoi ne pas créer une variable booléen par type d'attaque au lieu du typecode, niveau mémoire c'est un peu mieux qu'un gros int
+//genre un truc ainsi, qui serait stocké dans la database, au lieu du typecode,  des interrupteurs différents???
+	/*bool a_normalok;
+	bool a_fireok;
+	bool a_fightingok;
+	bool a_waterok;
+	bool a_flyingok;
+	bool a_grassok;
+	bool a_poison;
+	bool a_electric;
+	bool a_ground;
+	bool a_psychic;
+	bool a_rock;
+	bool a_ice;
+	bool a_bug;
+	bool a_dragon;
+	bool a_ghost;
+	bool a_dark;
+	bool asteel;
+	bool a_fairy;*/
 
-}
+};
+
