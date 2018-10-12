a7fd29624bb5a62d40661698132a121327cb4cbc
Draffaco017 <43415865+Draffaco017@users.noreply.github.com>
2018-10-12 19:18:22 +0200

    Update Pokemoncombat.cpp

--- PokeLike/PokeLike/Pokemoncombat.cpp
+++ PokeLike/PokeLike/Pokemoncombat.cpp
@@ -31,18 +31,18 @@ int Pokemoncombat::pc_getvitcombat()
 {
 	return pc_vitcombat;
 }
-void Pokemoncombat::pc_getdegats(Attaque lattaque, int *matricecoef[], Pokemoncombat attaquant)
+void Pokemoncombat::pc_getdegats(Attaque lattaque, float matricecoef[17][17], Pokemoncombat attaquant)
 {
 	int coef;
 	coef = matricecoef[(*this).p_getnombretype()][lattaque.a_getnombretype()];
 	int pvperdu;
 	if (lattaque.a_getstataffecteeoff() == "atk")//atk phys
 	{
-		pvperdu = (attaquant.pc_getatkcombat()* lattaque.a_getpuissanceoff() / ((*this).pc_getdefcombat()));
+		pvperdu = coef*(attaquant.pc_getatkcombat()* lattaque.a_getpuissanceoff() / ((*this).pc_getdefcombat()));
 	}
 	else//atk spe
 	{
-		pvperdu = (attaquant.pc_getatkspecombat() * lattaque.a_getpuissanceoff() / ((*this).pc_getdefspecombat()));
+		pvperdu = coef*(attaquant.pc_getatkspecombat() * lattaque.a_getpuissanceoff() / ((*this).pc_getdefspecombat()));
 	}
 	ps_pvrestant -= pvperdu;
 	if (ps_pvrestant < 0)
@@ -51,14 +51,26 @@ void Pokemoncombat::pc_getdegats(Attaque lattaque, int *matricecoef[], Pokemonco
 	}
 	//std::cout<<"pv restant :"<<ps_pvrestant<<std::endl;
 }
-void Pokemoncombat::pc_setup(Attaque lattaque)//multiplier la stat affecté par le boost par le coef
+void Pokemoncombat::pc_setupself(Attaque lattaque)//multiplier la stat affectÃ© par le boost par le coef
 {
 	if (lattaque.a_getstataffecteeset() == "atk")
 	{
-		pc_atkspecombat *= lattaque.a_getpuissanceset();
+		pc_atkcombat *= lattaque.a_getpuissanceset();
 	}
 	if (lattaque.a_getstataffecteeset() == "atkspe")
 	{
 		pc_atkspecombat *= lattaque.a_getpuissanceset();
+	}
+	if (lattaque.a_getstataffecteeset() == "def")
+	{
+		pc_defcombat *= lattaque.a_getpuissanceset();
+	}
+	if (lattaque.a_getstataffecteeset() == "defspe")
+	{
+		pc_defspecombat *= lattaque.a_getpuissanceset();
+	}
+	if (lattaque.a_getstataffecteeset() == "vit")
+	{
+		pc_vitcombat *= lattaque.a_getpuissanceset();
 	}
 }
